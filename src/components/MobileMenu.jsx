30dc893afa5b5cb9fff934378934630cc03564c3
jeyymss <jaguilar@gbox.adnu.edu.ph>
2025-03-16 13:27:16 +0800

    fix

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -8,6 +8,7 @@ export const MobileMenu = ({ menuOpen, setMenuOpen }) => {
     ${menuOpen ? "h-screen opacity-100 pointer-events-auto"
                : "h-0 opacity-0 pointer-events-none"
     }
+    
     `}>
         <button onClick={() => setMenuOpen(false)} className='absolute top-6 right-6 text-white text-3xl focus:outline-none cursor-pointer'
         aria-label='Close Menu'
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ export const Navbar = ({ menuOpen, setMenuOpen }) => {
 
   return (
     <nav className='fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blue-lg border-b border-white/10 shadow-lg'>
-        <div className='max-w-5xl mx-auto px-4'>
+        <div className='max-w-7xl mx-auto px-4'>
             <div className='flex justify-between items-center h-16'>
                 <a href='#home' className='font-mono text-xl font-bold text-white'> Pure <span className='text-blue-500'>Infinity</span>
                 </a>
