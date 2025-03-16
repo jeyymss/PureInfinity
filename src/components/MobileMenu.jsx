8305f247f30c5375e2fd112762aecdf739660297
jeyymss <jaguilar@gbox.adnu.edu.ph>
2025-03-16 16:46:15 +0800

    fix

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -3,7 +3,8 @@ import React, { useEffect } from 'react'
 export const MobileMenu = ({ menuOpen, setMenuOpen }) => {
 
   return (
-    <div className={`fixed top-0 left-0 w-full bg-[rgba(10, 10, 10, 0.8)] z-40 flex flex-col items-center justify-center transition-all duration-300 ease-in-out
+    <div className={`fixed top-0 left-0 w-full bg-[rgba(10,10,10,0.8)] z-40 flex flex-col items-center justify-center
+                     transition-all duration-300 ease-in-out
 
     ${menuOpen ? "h-screen opacity-100 pointer-events-auto"
                : "h-0 opacity-0 pointer-events-none"
@@ -24,6 +25,10 @@ export const MobileMenu = ({ menuOpen, setMenuOpen }) => {
         ${menuOpen ? "opacity-100 translate-y-0"
                    : "opacity-0 translate-y-5"}  
         `}> About </a>
+        <a href="#about" onClick={() => setMenuOpen(false)} className={`text-2xl font-semibold text-white my-4 transform transition-transform duration-300
+        ${menuOpen ? "opacity-100 translate-y-0"
+                   : "opacity-0 translate-y-5"}  
+        `}> Services </a>
         <a href="#contact" onClick={() => setMenuOpen(false)} className={`text-2xl font-semibold text-white my-4 transform transition-transform duration-300
         ${menuOpen ? "opacity-100 translate-y-0"
                    : "opacity-0 translate-y-5"}  
