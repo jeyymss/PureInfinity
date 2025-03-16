86566d511d5f466f039b8396084386a937745088
jeyymss <jaguilar@gbox.adnu.edu.ph>
2025-03-16 17:06:59 +0800

    fix

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -27,10 +27,10 @@ const About = () => {
         className='min-h-screen flex items-center justify-center py-20'>
         <RevealOnSroll>
         <div className='max-w-3xl mx-auto px-4'>
-            <h2 className='text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center'> About Us</h2>
+            <h2 className='text-4xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center'> About Us</h2>
 
             <div className='flex justify-center text-center mb-5'>
-              <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h1>
+              <h1 className='text-1xl mb-10'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h1>
             </div>
             <div className='grid grid-cols-2 md:grid-cols-3 gap-6'>
               <div className='p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all'>
--- src/components/sections/Services.jsx
+++ src/components/sections/Services.jsx
@@ -27,7 +27,7 @@ const Services = () => {
         className='min-h-screen flex items-center justify-center py-20'>
         <RevealOnSroll>
         <div className='max-w-7xl mx-auto px-4'>
-            <h2 className='text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center'> Services </h2>
+            <h2 className='text-4xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center'> Services </h2>
 
             <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-8'>
 
